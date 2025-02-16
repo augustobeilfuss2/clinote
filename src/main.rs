016c8023fcbb76fc8248a8b175af32f4559a8967
augustobeilfuss2 <augustobeilfuss2@gmail.com>
2025-02-16 18:35:25 -0300

    added print notes

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,18 @@ use std::env;
 use std::fs;
 use dirs::home_dir;
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{Write, Read};
 
 
 fn main() {
 
     let args: Vec<String>= env::args().collect();
-   // let note: Note = Note::new(&args, full_path); 
     let config_dir: &str = "/.config/clinotes/notes/";
     match home_dir() {
         Some(home) => {
 
             let full_path = format!("{}{}", home.display(), config_dir);
-            println!("{}", full_path);
+            //println!("a{}", full_path);
             fs::create_dir_all(&full_path);
             Note::new(&args, &full_path);
         }
@@ -31,24 +30,36 @@ struct Note {
 }
 
 impl Note {
-    pub fn new(args: &[String], full_path: &String) -> Note{
+    pub fn new(args: &[String], full_path: &String){
     
-        if args.len() < 3 {
-            panic!("Poucos arguementos")
-        }
-       
-        let file:String = args[1].clone();
-        let message:String = args[2].clone();
-        let file_full_path = format!("{}{}", full_path, file);
-        println!("{}", file_full_path);
-        let new_note = OpenOptions::new()
-        .create(true) // Create the file if it doesn't exist
-        .append(true) // Append to the file instead of overwriting
-        .open(file_full_path);
-        writeln!(new_note.expect("REASON"), "{}", message);
-       
+        if args.len() == 2 {
+            let file:String = args[1].clone();
+            let file_full_path = format!("{}{}", full_path, file);
+            println!("{}", file_full_path);
 
-        Note {file, message}
+            let read_note = OpenOptions::new()
+            .read(true)
+            .open(file_full_path);
+            let mut content = String::new();
+            read_note.expect("REASON").read_to_string(&mut content);
+            println!("---{}--- \n {}", file, content)
+        }
+
+        if args.len() == 3 {
+            let file:String = args[1].clone();
+            let message:String = args[2].clone();
+            let file_full_path = format!("{}{}", full_path, file);
+            println!("{}", file_full_path);
+
+            let new_note = OpenOptions::new()
+            .create(true) 
+            .append(true)
+            .open(file_full_path);
+            writeln!(new_note.expect("REASON"), "{}", message);
+           
+        }
+
+
     
     }
 
